765a76b9e0fb1d02091daf8cf6fbe31f849fad29
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-25 13:44:46 -0600

    feat: Variables dia 2, ejercicio Detalle de una persona

---
+++ src/_02variables/ReglasNombresVariables.java
@@ -0,0 +1,12 @@
+package _02variables;
+
+public class ReglasNombresVariables {
+    public static void main(String[] args) {
+        String nombreCompleto = "Juan Carlos Lopez Carrillo";
+        String nombreAbreviado = "";
+
+        nombreAbreviado = "JCLC";
+        System.out.println("nombreCompleto = " + nombreCompleto);
+        System.out.println("nombreAbreviado = " + nombreAbreviado);
+    }
+}
---
+++ src/_02variables/TiposDatos.java
@@ -0,0 +1,30 @@
+package _02variables;
+
+public class TiposDatos {
+    public static void main(String[] args) {
+        //Tipos de Datos en Java
+        //Enteros
+        byte tipoByte = 127;
+        short tipoShort = 32000;
+        int tipoInt = 1223224343;
+        long tipoLong = 1223243343344344444L;
+
+        System.out.println("tipoByte = " + tipoByte);
+        System.out.println("tipoShort = " + tipoShort);
+        System.out.println("tipoInt = "+ tipoInt);
+        System.out.println("tipoLong = " + tipoLong);
+        //Flotantes
+
+        float tipoFloat = 3.1416F;
+        double tipoDouble = 434.343435D;
+        System.out.println("tipoFloat = " + tipoFloat);
+        System.out.println("tipoDouble = " + tipoDouble);
+        //Char
+        char tipoChar = 44;
+        System.out.println("tipoChar = " + tipoChar);
+        //Boolean
+
+        boolean valorBinario = true;
+        System.out.println("valorBinario = " + valorBinario);
+    }
+}
