21fff526e0b4e8c7a37b597e16aee84432517488
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-25 04:31:31 -0600

    feat: Variables dia 2, ejercicio

---
+++ src/_02variables/DetalleLibro.java
@@ -0,0 +1,14 @@
+package _02variables;
+
+public class DetalleLibro {
+    public static void main(String[] args) {
+        String titulo = "Star Wars";
+        int anioPublicacion = 1999;
+        boolean libroDisponible = false;
+        double precio = 25.74;
+        System.out.println("titulo = " + titulo);
+        System.out.println("anioPublicacion = " + anioPublicacion);
+        System.out.println("libroDisponible = " + libroDisponible);
+        System.out.println("precio = " + precio);
+    }
+}
