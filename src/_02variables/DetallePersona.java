3099076e9ad744dad81c8d81b376fc73e15eb4a9
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-25 04:38:45 -0600

    feat: Variables dia 2, ejercicio Detalle de una persona

---
+++ src/_02variables/DetallePersona.java
@@ -0,0 +1,17 @@
+package _02variables;
+
+public class DetallePersona {
+    public static void main(String[] args){
+        String nombreCompleto = "Moctezuma Quetz";
+        int edad = 33;
+        double altura = 1.75;
+        String paisOrigen = "Mexico";
+        char estadoCivil = 'c';
+
+        System.out.println("nombreCompleto = " + nombreCompleto);
+        System.out.println("edad = " + edad);
+        System.out.println("altura = " + altura);
+        System.out.println("paisOrigen = " + paisOrigen);
+        System.out.println("estadoCivil = " + estadoCivil);
+    }
+}
