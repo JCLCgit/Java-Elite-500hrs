fbc72e49c4e4571f3b508e9a0515eb56644335bd
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-25 04:15:01 -0600

    feat: Varibales dia 2

---
+++ src/Variables/Variables.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args){
+        int edad = 31;
+        double precio = 3434.44;
+        boolean disponible = true;
+        char genero;
+        genero = 'M';
+
+        System.out.println("edad = " + edad);
+        System.out.println("precio = " + precio);
+        System.out.println("disponible = " + disponible);
+        System.out.println("genero = " + genero);
+    }
+}
