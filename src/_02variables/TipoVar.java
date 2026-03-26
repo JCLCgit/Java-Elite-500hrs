9573644cd9feeeae246874e0d7da20db1c90ed49
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-26 02:30:40 -0600

    feat: Variables tipo VAR. Día 3

---
+++ src/_02variables/TipoVar.java
@@ -0,0 +1,13 @@
+package _02variables;
+
+public class TipoVar {
+    public static void main(String[] args) {
+        var nombre = "HOla";
+        var precio = 334.5F;
+        var isOpen = true;
+        isOpen = false;
+        System.out.println("nombre = " + nombre);
+        System.out.println("precio = " + precio);
+        System.out.println("isOpen = " + isOpen);
+    }
+}
