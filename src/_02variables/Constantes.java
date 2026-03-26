a94ec3271f5e32ede1b7f24b4c4928f0f4411c5b
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-26 02:43:25 -0600

    feat: Constantes. Día 3

---
+++ src/_02variables/Constantes.java
@@ -0,0 +1,14 @@
+package _02variables;
+
+public class Constantes {
+    public static void main(String[] args) {
+        final int DIAS_SEMANA = 7;
+        final var PI = 3.1416;
+        final var MENSAJE = "Bienvenidos";
+
+        System.out.println(" DIAS_SEMANA = " + DIAS_SEMANA );
+        System.out.println("PI = " + PI);
+        System.out.println("PI = " + Math.PI);
+        System.out.println("MENSAJE = " + MENSAJE);
+    }
+}
