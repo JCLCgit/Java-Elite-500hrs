afd44a8183024ebbb96670d2828806bd0dd83774
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-26 02:37:21 -0600

    feat: Concatenación de Cadenas básica. Día 3

---
+++ src/_02variables/ConcatenacionCadenas.java
@@ -0,0 +1,12 @@
+package _02variables;
+
+public class ConcatenacionCadenas {
+    public static void main(String[] args){
+        System.out.println("*** Concatenación Cadenas ***");
+        var nombre = "Peter";
+        var apellido = "Parker";
+        var nombreCompleto = nombre+" "+apellido;
+        System.out.println("nombreCompleto = " + nombreCompleto);
+
+    }
+}
