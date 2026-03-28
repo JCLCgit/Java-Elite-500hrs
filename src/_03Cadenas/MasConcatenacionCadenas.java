d2fc16c15b4909ebc9d757f4ce3e9dfe9b91d211
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-28 21:40:55 +0100

    feat: Concatenación de cadenas. Día 5

---
+++ src/_03Cadenas/MasConcatenacionCadenas.java
@@ -0,0 +1,13 @@
+package _03Cadenas;
+
+public class MasConcatenacionCadenas {
+    public static void main(String[] args){
+        var cadena1 = "El señor";
+        var cadena2 = "de los anillos";
+        String cadena3 = null;
+        System.out.println("cadena1 = " + cadena1);
+        System.out.println("cadena2 = " + cadena2);
+        System.out.println("cadena3 = " + cadena3);
+
+    }
+}
