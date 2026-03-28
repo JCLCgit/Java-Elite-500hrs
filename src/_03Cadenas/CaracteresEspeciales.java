62938b927171b9e9f0cb59838a239a8bd6885cd4
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-28 22:51:50 +0100

    feat: Caracteres especiales. Día 5

---
+++ src/_03Cadenas/CaracteresEspeciales.java
@@ -0,0 +1,12 @@
+package _03Cadenas;
+
+public class CaracteresEspeciales {
+    public static void main(String[] args) {
+        var cadena = "Hola Mundo";
+        System.out.println("cadena\n mostrada = " + cadena);
+        System.out.println("\tcadena\tmostrada2 = " + cadena);
+        System.out.println("cadena\' = " + cadena);
+        System.out.println("cadena \" = " + cadena);
+        System.out.println("cadena \\ = " + cadena);
+    }
+}
