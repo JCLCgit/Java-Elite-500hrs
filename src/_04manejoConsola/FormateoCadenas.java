978bf665ba83d5e36df51469e01b7c3d3567e0fe
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-30 14:04:03 +0200

    feat: Formateo de Cadenas. Día 7

---
+++ src/_04manejoConsola/FormateoCadenas.java
@@ -0,0 +1,20 @@
+package _04manejoConsola;
+
+public class FormateoCadenas {
+    public static void main(String[] args) {
+        System.out.println("***Formateo de Cadenas***");
+        System.out.println("**Utilizando String.format" +
+                "y printf**");
+        var nombre = "JCLC";
+        var edad = 32;
+        var salario = 37500.8556;
+
+        var mensajeSalida = String.format("Nombre: %s, Edad: %d, " +
+                "Salario: %.2f", nombre,edad,salario);
+        System.out.println("mensajeSalida = " + mensajeSalida);
+
+        System.out.printf("Nombre: %s, Edad: %d," +
+                          "Salario: %.2f\n", nombre,edad,salario);
+
+    }
+}
