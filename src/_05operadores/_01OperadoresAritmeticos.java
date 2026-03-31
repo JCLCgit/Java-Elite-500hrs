b1a58132977a670fee88c24a13610c3a680bb2e5
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-31 21:56:41 +0200

    feat: Operaciones. Día 8

---
+++ src/_05operadores/_01OperadoresAritmeticos.java
@@ -0,0 +1,19 @@
+package _05operadores;
+
+public class _01OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int a = 5, b = 2, resultado;
+        resultado = a+b;
+        System.out.println("resultado suma = " + resultado);
+        resultado = a-b;
+        System.out.println("resultado resta = " + resultado);
+        resultado = a*b;
+        System.out.println("resultado multiplicación = " + resultado);
+        resultado = a%b;
+        System.out.println("resultado módulo = " + resultado);
+
+
+
+
+    }
+}
