e8be8fd370c9c806a8cc480ce74a7be33b394e9a
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-03-31 22:06:19 +0200

    feat: Operadores Unarios. Día 8

---
+++ src/_05operadores/_02OperadoresUnarios.java
@@ -0,0 +1,19 @@
+package _05operadores;
+
+public class _02OperadoresUnarios {
+    public static void main(String[] args) {
+        System.out.println("***Operadores Unarios***");
+        int a = 7, b = 4, resultado;
+        System.out.println("VALOR ORIGINAL a = " + a);
+        System.out.println("VALOR ORIGINAL b = " + b);
+        resultado = +a;
+        System.out.println("+a = " + resultado);
+        resultado = ++a;
+        System.out.println("++a = " + resultado);
+        resultado = a++;
+        System.out.println("resutlado = a++ = " + resultado);
+        System.out.println("a++ aplicado = " + a);
+
+
+    }
+}
