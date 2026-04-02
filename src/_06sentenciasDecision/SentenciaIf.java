b928b3417f3deced0d8562313f13848befb6feba
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-02 14:19:55 +0200

    feat: Sentencia If - else, else-if. Día 10

--- src/_06sentenciasDecision/SentenciaIf.java
+++ src/_06sentenciasDecision/SentenciaIf.java
@@ -6,7 +6,9 @@ public class SentenciaIf {
 
         if (edad >= 18)
             System.out.println("Eres mayor de edad");
-        else
+        else if (edad >= 13 && edad < 18) {
+            System.out.println("Eres un adolescente");
+        } else
             System.out.println("eres menor de edad");
     }
 }
