79fa2e82532a1bdfdb28335a70d9729824e46b99
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-02 14:15:24 +0200

    feat: Sentencia If. Día 10

---
+++ src/_06sentenciasDecision/SentenciaIf.java
@@ -0,0 +1,10 @@
+package _06sentenciasDecision;
+
+public class SentenciaIf {
+    public static void main(String[] args) {
+        var edad = 15;
+
+        if (edad >= 18)
+            System.out.println("Eres mayor de edad");
+    }
+}
