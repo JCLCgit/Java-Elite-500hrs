5faed4ceb8f366bcf87babc6fdb5ad68be1da55c
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-05 10:20:51 +0200

    feat: Ejercicio sistema de calificaciones if-else . Día 13

---
+++ src/_06sentenciasDecision/_14SistemaCalificaciones.java
@@ -0,0 +1,28 @@
+package _06sentenciasDecision;
+
+import java.util.Random;
+
+public class _14SistemaCalificaciones {
+    public static void main(String[] args) {
+        double min = -1.0, max = 10.0;
+
+        System.out.println("**Sistema de Calificaciones**");
+        Random aletorio = new Random();
+
+        var calificacion = aletorio.nextFloat()*(max)+min;
+        System.out.printf("calificacion = %.2f\n", calificacion);
+
+        if (calificacion <= 10 && calificacion > 9 )
+            System.out.println("CALIFICACIÓN -> A");
+        else if (calificacion < 9 && calificacion >= 8)
+            System.out.println("CALIFICACIÓN -> B");
+        else if (calificacion < 8 && calificacion >= 7)
+            System.out.println("CALIFICACIÓN -> C");
+        else if (calificacion < 7 && calificacion >= 6)
+            System.out.println("CALIFICACIÓN -> D");
+        else if (calificacion < 6 && calificacion >= 0)
+            System.out.println("CALIFICACIÓN -> F");
+        else
+            System.out.println("CALIFICACIÓN INVáLIDA");
+    }
+}
