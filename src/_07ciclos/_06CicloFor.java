0fefe9a2f479750392a61d8bcf8877fc65e1eea7
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-06 14:08:03 +0200

    feat: Ciclo For. Día 14

---
+++ src/_07ciclos/_06CicloFor.java
@@ -0,0 +1,11 @@
+package _07ciclos;
+
+public class _06CicloFor {
+    public static void main(String[] args) {
+        System.out.println("**Ciclo For**");
+
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.print(i + ", ");
+        }
+    }
+}
