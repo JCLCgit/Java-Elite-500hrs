0a9955afb9504ba2bcff197836836bf75d3b80a7
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-07 22:00:32 +0200

    feat: Ejercicio cajero automático, implementación opciones con Switch. Día 15

--- src/_07ciclos/_10CajeroAutomatico.java
+++ src/_07ciclos/_10CajeroAutomatico.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class _10CajeroAutomatico {
     public static void main(String[] args) {
         System.out.println("**Cajero automático**");
-        final var SALDO_CUENTA = 1200.50;
+        var saldoCuenta = 1200.50;
 
         var sistemaCajero = false;
         var scanner = new Scanner(System.in);
-
+        var opcion = 0;
+        var monto = 0.0;
         while (!sistemaCajero){
             System.out.printf("""
                     \nOperaciones que puede realizar:
@@ -19,6 +20,26 @@ public class _10CajeroAutomatico {
                     4. Salir
                     """);
             System.out.print("Escoge una opción: ");
+            opcion = Integer.parseInt(scanner.nextLine());
+            switch (opcion){
+                case 1 -> System.out.printf("\nSu saldo es: %.3f",saldoCuenta);
+                case 2 -> {
+                    System.out.print("Ingresa el monto a retirar: ");
+                    monto = Float.parseFloat(scanner.nextLine());
+                    saldoCuenta -= monto;
+                }
+                case 3 -> {
+                    System.out.print("Ingresa el monto a depositar: ");
+                    monto = Float.parseFloat(scanner.nextLine());
+                    saldoCuenta += monto;
+                }
+                case 4 -> {
+                    System.out.println("Saliendo del sistema...");
+                    sistemaCajero = true;
+                }
+                default -> System.out.println("Ingresa una opción válida");
+
+            }
 
         }
     }
