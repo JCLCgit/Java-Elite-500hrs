b3e7a2265b7296ff2f72649b70e6530d2001fc5e
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-08 22:25:43 +0200

    feat: Ejercicio calculadora, completado. Día 16

--- src/_07ciclos/_11Calculadora.java
+++ src/_07ciclos/_11Calculadora.java
@@ -8,7 +8,9 @@ public class _11Calculadora {
         var scanner = new Scanner(System.in);
         var opcion = 0;
         var encendidoCalculadora = false;
-
+        var numeroA = 0.0;
+        var numeroB = 0.0;
+        var resultado = 0.0;
         while (!encendidoCalculadora){
             System.out.printf("""
                     \n1. Suma
@@ -20,7 +22,43 @@ public class _11Calculadora {
             System.out.print("Ingresa una opción: ");
             opcion = Integer.parseInt(scanner.nextLine());
             switch (opcion){
-
+                case 1 -> {
+                    System.out.printf("Ingresa tu primer número: ");
+                    numeroA = Float.parseFloat(scanner.nextLine());
+                    System.out.printf("Ingresa tu segundo número: ");
+                    numeroB = Float.parseFloat(scanner.nextLine());
+                    resultado = numeroA + numeroB;
+                    System.out.println("resultado = " + resultado);
+                }
+                case 2 -> {
+                    System.out.printf("Ingresa tu primer número: ");
+                    numeroA = Float.parseFloat(scanner.nextLine());
+                    System.out.printf("Ingresa tu segundo número: ");
+                    numeroB = Float.parseFloat(scanner.nextLine());
+                    resultado = numeroA - numeroB;
+                    System.out.println("resultado = " + resultado);
+                }
+                case 3 -> {
+                    System.out.printf("Ingresa tu primer número: ");
+                    numeroA = Float.parseFloat(scanner.nextLine());
+                    System.out.printf("Ingresa tu segundo número: ");
+                    numeroB = Float.parseFloat(scanner.nextLine());
+                    resultado = numeroA * numeroB;
+                    System.out.println("resultado = " + resultado);
+                }
+                case 4 -> {
+                    System.out.printf("Ingresa tu primer número: ");
+                    numeroA = Float.parseFloat(scanner.nextLine());
+                    System.out.printf("Ingresa tu segundo número: ");
+                    numeroB = Float.parseFloat(scanner.nextLine());
+                    resultado = numeroA / numeroB;
+                    System.out.println("resultado = " + resultado);
+                }
+                case 5 -> {
+                    System.out.printf("Saliendo del sistema...");
+                    encendidoCalculadora = true;
+                }
+                default -> System.out.println("¡Opción inválida!");
             }
         }
     }
