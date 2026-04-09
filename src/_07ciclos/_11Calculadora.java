b07f77558a9175878fb9996f80343838396ca59e
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-09 20:23:36 +0200

    feat: Ejercicio calculadora, mejorado. Día 16

--- src/_07ciclos/_11Calculadora.java
+++ src/_07ciclos/_11Calculadora.java
@@ -21,38 +21,33 @@ public class _11Calculadora {
                     %n""");
             System.out.print("Ingresa una opción: ");
             opcion = Integer.parseInt(scanner.nextLine());
+            if(opcion >= 1 && opcion <= 4){
+                System.out.printf("Ingresa tu primer número: ");
+                numeroA = Float.parseFloat(scanner.nextLine());
+                System.out.printf("Ingresa tu segundo número: ");
+                numeroB = Float.parseFloat(scanner.nextLine());
+            }
             switch (opcion){
                 case 1 -> {
-                    System.out.printf("Ingresa tu primer número: ");
-                    numeroA = Float.parseFloat(scanner.nextLine());
-                    System.out.printf("Ingresa tu segundo número: ");
-                    numeroB = Float.parseFloat(scanner.nextLine());
                     resultado = numeroA + numeroB;
-                    System.out.println("resultado = " + resultado);
+                    System.out.println("resultado SUMA= " + resultado);
                 }
                 case 2 -> {
-                    System.out.printf("Ingresa tu primer número: ");
-                    numeroA = Float.parseFloat(scanner.nextLine());
-                    System.out.printf("Ingresa tu segundo número: ");
-                    numeroB = Float.parseFloat(scanner.nextLine());
                     resultado = numeroA - numeroB;
-                    System.out.println("resultado = " + resultado);
+                    System.out.println("resultado RESTA= " + resultado);
                 }
                 case 3 -> {
-                    System.out.printf("Ingresa tu primer número: ");
-                    numeroA = Float.parseFloat(scanner.nextLine());
-                    System.out.printf("Ingresa tu segundo número: ");
-                    numeroB = Float.parseFloat(scanner.nextLine());
                     resultado = numeroA * numeroB;
-                    System.out.println("resultado = " + resultado);
+                    System.out.println("resultado MULTIPLICACIÓN= " + resultado);
                 }
                 case 4 -> {
-                    System.out.printf("Ingresa tu primer número: ");
-                    numeroA = Float.parseFloat(scanner.nextLine());
-                    System.out.printf("Ingresa tu segundo número: ");
-                    numeroB = Float.parseFloat(scanner.nextLine());
-                    resultado = numeroA / numeroB;
-                    System.out.println("resultado = " + resultado);
+                    if (numeroB != 0){
+                        resultado = numeroA / numeroB;
+                        System.out.println("resultado DIVISIÓN = " + resultado);
+                    }
+                    else
+                        System.out.println("Error: División entre CERO");
+
                 }
                 case 5 -> {
                     System.out.printf("Saliendo del sistema...");
