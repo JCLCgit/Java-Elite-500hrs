74bba20806f01c08f212205727d6a640ab804fc4
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-10 21:41:34 +0200

    feat: Declaración variable de arrays. Día 18

---
+++ src/_08arreglos/_01Arreglos.java
@@ -0,0 +1,9 @@
+package _08arreglos;
+
+public class _01Arreglos {
+    public static void main(String[] args) {
+        int[] arreglos = new int[4];
+        arreglos[0] = 2;
+        System.out.println(arreglos[0]);
+    }
+}
