ef2c330ff8e20b3717dd55663e044ba9e05a2a70
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-10 21:44:03 +0200

    feat: Asignación de valores en arrays. Día 18

--- src/_08arreglos/_01Arreglos.java
+++ src/_08arreglos/_01Arreglos.java
@@ -4,6 +4,10 @@ public class _01Arreglos {
     public static void main(String[] args) {
         int[] arreglos = new int[4];
         arreglos[0] = 2;
+        arreglos[1] = 4;
+        arreglos[2] = 7;
+        arreglos[3] = 9;
+
         System.out.println(arreglos[0]);
     }
 }
