1bbf538fedb90f01e82f53af09b32bf2d984af6a
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-10 21:17:08 +0200

    feat: Palabra reservada Break y Continue. Día 18

---
+++ src/_07ciclos/_15BreakContinue.java
@@ -0,0 +1,28 @@
+package _07ciclos;
+
+import java.util.Scanner;
+
+public class _15BreakContinue {
+    public static void main(String[] args) {
+        System.out.println("**Uso de Break y Continue**");
+        var scanner = new Scanner(System.in);
+        System.out.print("Ingresa un número limite: ");
+        int limite = scanner.nextInt();
+        //Imprimir únicamente el primer número par
+        for (int numero = 1; numero <= limite ; numero++) {
+            if(numero % 2 == 0){
+                System.out.printf("Número antes de romper el ciclo: %d%n",numero);
+                break;
+            }
+        }
+        System.out.println("Números impares");
+        for (int numeroDos = 1; numeroDos < limite; numeroDos++) {
+            if(numeroDos % 2 == 1 ){
+                System.out.print(" "+ numeroDos);
+
+            }
+
+        }
+
+    }
+}
