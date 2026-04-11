efa59641e926579c5739522c18d30700cc202988
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-11 23:03:47 +0200

    feat: Otras declaraciones e impresión simplificada de arrays. Día 19

--- src/_08arreglos/_01Arreglos.java
+++ src/_08arreglos/_01Arreglos.java
@@ -3,6 +3,8 @@ package _08arreglos;
 public class _01Arreglos {
     public static void main(String[] args) {
         int[] arreglos = new int[4];
+        int[] arregloDos = {100,200,300};
+        var arregloTres = new int[]{50,70,80};
         arreglos[0] = 2;
         arreglos[1] = 4;
         arreglos[2] = 7;
@@ -12,5 +14,12 @@ public class _01Arreglos {
         System.out.println("Valor 2 ->" + arreglos[1]);
         System.out.println("Valor 3 ->" + arreglos[2]);
         System.out.println("Valor 4 ->" + arreglos[3]);
+
+        for (int i = 0; i < arregloDos.length; i++) {
+            System.out.println("Valor 1 ->" + arregloDos[i]);
+        }
+        for (int i = 0; i < arregloTres.length; i++) {
+            System.out.println("Valor 1 ->" + arregloTres[i]);
+        }
     }
 }
