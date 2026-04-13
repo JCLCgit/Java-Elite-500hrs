df889ea0de9b470983f32c55faa7f07052a79480
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-13 10:30:43 +0200

    feat: Introducción a funciones. Día 21

---
+++ src/_10funciones/_01IntroduccionFunciones.java
@@ -0,0 +1,15 @@
+package _10funciones;
+
+public class _01IntroduccionFunciones {
+
+    static void imprimir (String mensaje){
+        System.out.println("mensaje = " + mensaje);
+    }
+
+
+    public static void main(String[] args) {
+        String frase = "Bienvenido a las funciones";
+        imprimir(frase);
+        imprimir("Java rules");
+    }
+}
