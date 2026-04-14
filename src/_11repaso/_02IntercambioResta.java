6ea825de67173bf8868207ad18607f09323b694c
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-14 23:10:30 +0200

    feat: Intercambio valores sin tercera variable, primer cambio. Día 22

---
+++ src/_11repaso/_02IntercambioResta.java
@@ -0,0 +1,15 @@
+package _11repaso;
+
+public class _02IntercambioResta {
+    public static void main(String[] args) {
+
+        System.out.println("**Intercambio valores con variable temporal**");
+        int a = 200, b = 500, c;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a + b;
+        System.out.println("a = " + a);
+        b = a - b;
+        System.out.println("b = " + b);
+    }
+}
