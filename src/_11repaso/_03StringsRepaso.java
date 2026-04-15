0d68fd282c7f4c2c54ccd89bcc0a5f088450eef9
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-15 23:17:02 +0200

    feat: Práctica de Strings optimizado. Día 23

--- src/_11repaso/_03StringsRepaso.java
+++ src/_11repaso/_03StringsRepaso.java
@@ -2,15 +2,15 @@ package _11repaso;
 
 public class _03StringsRepaso {
 
-    static String crearMensaje(String mensaje){
-        String formato = "<<>>";
-        return formato.substring(0, 2) + mensaje + formato.substring(2, 4);
+    static String crearMensaje(String carecteres, String mensaje){
+
+        return carecteres.substring(0, 2) + mensaje + carecteres.substring(2, 4);
 
     }
     public static void main(String[] args) {
 
         String mensaje = "Me llamo Juan Carlos";
-        var resultado = crearMensaje(mensaje);
+        var resultado = crearMensaje("{{}}",mensaje);
         System.out.println("resultado = " + resultado);
     }
 }
