8a088724e072dd3eb2f398b805625b469de636c0
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-16 15:02:55 +0200

    feat: Ejercicio Lógica Coding Bat NearHundred (comienzo). Día 24

---
+++ src/_11repaso/_04DistanciaNumeros.java
@@ -0,0 +1,19 @@
+package _11repaso;
+
+import java.util.Random;
+
+public class _04DistanciaNumeros {
+    public static void main(String[] args) {
+
+        Random aleatorio = new Random();
+
+        int numero;
+        for (int i = 0; i < 10; i++) {
+            numero = aleatorio.nextInt(201) + 1;
+            if ( Math.abs(200-numero ) <= 10  || ){
+
+            }
+        }
+
+    }
+}
