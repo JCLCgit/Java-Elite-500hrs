1a70d9a87b0ed93fa1b5f4a7bce7731f49fe418c
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-20 22:23:26 +0200

    feat: Ejercicio Lógica Coding mixStart (ajuste). Día 26

--- src/_11repaso/_08MixStart.java
+++ src/_11repaso/_08MixStart.java
@@ -3,10 +3,17 @@ package _11repaso;
 public class _08MixStart {
     static boolean mixStart(String cadena){
 
-
-        return;
+        return (cadena.strip().substring(1,2).equals("ix"));
     }
     public static void main(String[] args) {
+
+        String cadena = "mix clasicos";
+
+        System.out.println("cadena = " + mixStart(cadena));
+        System.out.println("cadena = " + mixStart("mix snacks"));
+        System.out.println("cadena = " + mixStart("pix snacks"));
+        System.out.println("cadena = " + mixStart("piz snacks"));
+
 
     }
 }
