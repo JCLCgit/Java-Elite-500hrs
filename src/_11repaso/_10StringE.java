ad40228ab36bce7db8a31e46cedf3086f3114d39
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-25 23:17:59 +0200

    feat: Ejercicio Lógica StringE, contabilizando letras "e" (comienzo). Día 28

---
+++ src/_11repaso/_10StringE.java
@@ -0,0 +1,25 @@
+package _11repaso;
+
+public class _10StringE {
+    /*
+
+Return true if the given string contains between 1 and 3 'e' chars.
+
+
+stringE("Hello") → true
+stringE("Heelle") → true
+stringE("Heelele") → false
+     */
+    static boolean stringE(String str) {
+        int contador = 0;
+        for (int i = 0; i < str.length() ; i++) {
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String cadena = "hello";
+
+        stringE(cadena);
+    }
+}
