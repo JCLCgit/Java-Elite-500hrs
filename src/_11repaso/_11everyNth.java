9092b22dbcdef225899998cb26cccba4686cb5d4
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-26 19:06:03 +0200

    feat: Ejercicio Lógica everyNth(comienzo). Día 29

---
+++ src/_11repaso/_11everyNth.java
@@ -0,0 +1,26 @@
+package _11repaso;
+
+public class _11everyNth {
+
+    /*
+Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+everyNth("Miracle", 2) → "Mrce"
+everyNth("abcdefg", 2) → "aceg"
+everyNth("abcdefg", 3) → "adg
+     */
+    static String everyNth(String str, int n) {
+
+
+
+        return "Hello";
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Resultado = " + everyNth("Miracle", 2));
+        System.out.println("Resultado = " + everyNth("abcdefg", 2));
+        System.out.println("Resultado = " + everyNth("abcdefg", 3));
+
+
+    }
+}
