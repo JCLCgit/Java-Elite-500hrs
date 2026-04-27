8a04b0fcf00baae76e3ea7cf800bbdef6d3efe31
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-27 13:44:53 +0200

    feat: Ejercicio Lógica ParrotTrouble(completo). Día 30

--- src/_11repaso/_13ParrotTrouble.java
+++ src/_11repaso/_13ParrotTrouble.java
@@ -3,14 +3,23 @@ package _11repaso;
 public class _13ParrotTrouble {
     /*
 
-We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 
 
 parrotTrouble(true, 6) → true
 parrotTrouble(true, 7) → false
 parrotTrouble(false, 6) → false
      */
+    static boolean parrotTrouble(boolean talking, int hour) {
+
+        return (hour > 20 && hour < 7 && true ? true : false);
+    }
     public static void main(String[] args) {
+
+        System.out.println("parrotTrouble(true,6) = " + parrotTrouble(true,6));
+        System.out.println("parrotTrouble(true,7) = " + parrotTrouble(true,7));
+        System.out.println("parrotTrouble(false,6) = " + parrotTrouble(false,6));
 
     }
 }
