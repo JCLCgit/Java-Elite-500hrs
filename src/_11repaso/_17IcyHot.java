6740e6dfcb942c6dc2ff812439068e5d925dc117
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-30 18:24:15 +0200

    feat: Ejercicio Lógica IcyHot(comienzo). Día 31

---
+++ src/_11repaso/_17IcyHot.java
@@ -0,0 +1,21 @@
+package _11repaso;
+
+public class _17IcyHot {
+    /*
+Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false
+     */
+    static boolean icyHot(int temp1, int temp2) {
+
+        
+    }
+    public static void main(String[] args) {
+
+        System.out.println("icyHot(120, -1) = " + icyHot(120, -1));
+        System.out.println("icyHot(-1, 120) = " + icyHot(-1, 120));
+        System.out.println("icyHot(2, 120) = " + icyHot(2, 120));
+    }
+}
