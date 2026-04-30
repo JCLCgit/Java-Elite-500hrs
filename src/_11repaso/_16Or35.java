35b65dff9e11f4cc30c461e2ed9bdbffd700a872
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-30 13:43:12 +0200

    feat: Ejercicio Lógica Or35(comienzo). Día 31

---
+++ src/_11repaso/_16Or35.java
@@ -0,0 +1,16 @@
+package _11repaso;
+
+public class _16Or35 {
+    /*
+    Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+    Use the % "mod" operator
+
+
+or35(3) → true
+or35(10) → true
+or35(8) → false
+     */
+    public static void main(String[] args) {
+
+    }
+}
