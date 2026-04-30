ad684d284be04d79819334ffe0beb9bad56aa51e
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-04-30 21:01:40 +0200

    feat: Ejercicio Lógica IcyHot(completo). Día 31

--- src/_11repaso/_17IcyHot.java
+++ src/_11repaso/_17IcyHot.java
@@ -10,7 +10,7 @@ icyHot(2, 120) → false
      */
     static boolean icyHot(int temp1, int temp2) {
 
-        
+        return ((temp1 > 100 && temp2 < 0) || (temp2 > 100 && temp1 < 0));
     }
     public static void main(String[] args) {
 
