b49a4a138f3aa0810e7f89579df91740f9ea941e
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-01 10:35:38 +0200

    feat: Ejercicio Lógica In3050(comienzo). Día 32

--- src/_11repaso/_20In3050.java
+++ src/_11repaso/_20In3050.java
@@ -3,14 +3,21 @@ package _11repaso;
 public class _20In3050 {
     /*
 
-Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+Given 2 int values, return true if they are both
+in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
 
 in3050(30, 31) → true
 in3050(30, 41) → false
 in3050(40, 50) → true
      */
+    static boolean in3050(int a, int b) {
+
+    }
     public static void main(String[] args) {
 
+        System.out.println("in3050() = " + in3050(30, 31));
+        System.out.println("in3050(30,41) = " + in3050(30,41));
+        System.out.println("in3050(40,50) = " + in3050(40,50));
 
     }
 }
