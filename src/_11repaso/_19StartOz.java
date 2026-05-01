4302b106b4d7012476fad4f2300e7936f7211024
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-01 09:36:49 +0200

    feat: Ejercicio Lógica StartOz(comienzo). Día 31

---
+++ src/_11repaso/_19StartOz.java
@@ -0,0 +1,21 @@
+package _11repaso;
+
+public class _19StartOz {
+    /*
+
+Given a string, return a string made of the first 2 chars (if present), however
+include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+
+startOz("ozymandias") → "oz"
+startOz("bzoo") → "z"
+startOz("oxx") → "o"
+     */
+    static String startOz(String str) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
