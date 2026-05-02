ef7cca1c0a7c32e1d6794c39f9d1e83f8d8cd149
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-02 21:28:02 +0200

    feat: Ejercicio Lógica SumDouble(completo). Día 33

---
+++ src/_11repaso/_22SumDouble.java
@@ -0,0 +1,24 @@
+package _11repaso;
+
+public class _22SumDouble {
+    /*
+    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+     */
+    static int sumDouble(int a, int b) {
+        int sum = 0;
+        if(a == b)
+            sum += 2 * (a + b);
+        else
+            sum = a + b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("sumDouble(1, 2) = " + sumDouble(1, 2));
+        System.out.println("sumDouble(3, 2) = " + sumDouble(3, 2));
+        System.out.println("sumDouble(2, 2) = " + sumDouble(2, 2));
+    }
+}
