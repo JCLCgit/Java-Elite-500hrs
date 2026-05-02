078a0ac8f20caca105aa9467fd13bb9f6ae3af9b
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-02 21:37:38 +0200

    feat: Ejercicio Lógica Makes10(comienzo). Día 33

---
+++ src/_11repaso/_24NotString.java
@@ -0,0 +1,21 @@
+package _11repaso;
+
+public class _24NotString {
+    /*
+Given a string, return a new string where "not " has been added to the front.
+However, if the string already begins with "not", return the string unchanged.
+Note: use .equals() to compare 2 strings.
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+     */
+    static String notString(String str) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("notS = " + notString("candy"));
+        System.out.println("notString(\"x\") = " + notString("x"));
+        System.out.println("notString(\"not bad\") = " + notString("not bad"));
+    }
+}
