0d17cf952825d16b49b75de26ef20ae39ed36748
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-02 20:49:11 +0200

    feat: Ejercicio Lógica In3050(completo). Día 33

--- src/_11repaso/_20In3050.java
+++ src/_11repaso/_20In3050.java
@@ -12,6 +12,13 @@ in3050(40, 50) → true
      */
     static boolean in3050(int a, int b) {
 
+        if ((a >= 30 && a <= 40) && (b >= 30 && b <= 40))
+            return true;
+        else if ((a >= 40 && a <= 50) && (b >= 40 && b <= 50)) {
+            return true;
+        }
+        else
+            return false;
     }
     public static void main(String[] args) {
 
