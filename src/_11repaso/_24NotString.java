c133828fec8c53717194e4de5f1b043d0e9e3c50
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-03 15:44:13 +0200

    feat: Ejercicio Lógica NotString(completo). Día 34

--- src/_11repaso/_24NotString.java
+++ src/_11repaso/_24NotString.java
@@ -12,6 +12,11 @@ notString("not bad") → "not bad"
      */
     static String notString(String str) {
 
+        if (str.length() >= 3 && str.substring(0,3).equals("not")){
+            return str;
+        }else
+            return "not " + str;
+
     }
     public static void main(String[] args) {
         System.out.println("notS = " + notString("candy"));
