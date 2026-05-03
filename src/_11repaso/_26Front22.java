4878f03211027b26c0c1a54e1028985bc52f1878
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-03 22:21:40 +0200

    feat: Ejercicio Lógica Front22(completo). Día 34

--- src/_11repaso/_26Front22.java
+++ src/_11repaso/_26Front22.java
@@ -11,11 +11,16 @@ front22("Ha") → "HaHaHa"
 front22("abc") → "ababcab"
      */
     static String front22(String str) {
-
+        String result;
+        if (str.length() >= 2){
+            result = str.substring(0, 2) + str + str.substring(0, 2);
+            return result;
+        } else
+            return str + str + str;
     }
     public static void main(String[] args) {
         System.out.println("front22(\"kitten\") = " + front22("kitten"));
-        System.out.println("front22(\"HaHaHa\") = " + front22("HaHaHa"));
+        System.out.println("front22(\"HaHaHa\") = " + front22("Ha"));
         System.out.println("front22(\"abc\") = " + front22("abc"));
     }
 }
