3073f70f9d5e20dffd8cf87c2edb4235c501ba97
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-03 15:56:48 +0200

    feat: Ejercicio Lógica Front22(comienzo). Día 34

---
+++ src/_11repaso/_26Front22.java
@@ -0,0 +1,21 @@
+package _11repaso;
+
+public class _26Front22 {
+    /*
+    Given a string, take the first 2 chars and return
+    the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki".
+    If the string length is less than 2, use whatever chars are there.
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+     */
+    static String front22(String str) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("front22(\"kitten\") = " + front22("kitten"));
+        System.out.println("front22(\"HaHaHa\") = " + front22("HaHaHa"));
+        System.out.println("front22(\"abc\") = " + front22("abc"));
+    }
+}
