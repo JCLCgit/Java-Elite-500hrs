67410b17db0980f2fd1ac215947a14d1afefff03
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-07 22:17:54 +0200

    feat: Ejercicios CodingBat String-1 MiddleThree(comienzo incompleto). Día 37

---
+++ src/_12repasoString1/_06MiddleThree.java
@@ -0,0 +1,14 @@
+package _12repasoString1;
+
+public class _06MiddleThree {
+    /*
+Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+The string length will be at least 3.
+middleThree("Candy") → "and"
+middleThree("and") → "and"
+middleThree("solving") → "lvi"
+     */
+    public static void main(String[] args) {
+
+    }
+}
