6f9887f14aaabde0809a0d7429d805d8ee01cd79
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-08 23:28:51 +0200

    feat: Ejercicios CodingBat String-1 MiddleThree(completo). Día 38

--- src/_12repasoString1/_06MiddleThree.java
+++ src/_12repasoString1/_06MiddleThree.java
@@ -8,7 +8,17 @@ middleThree("Candy") → "and"
 middleThree("and") → "and"
 middleThree("solving") → "lvi"
      */
+    static String middleThree(String str) {
+        int start = 0;
+        start = (str.length() - 3) / 2;
+        if (str.length() > 3)
+            return str.substring(start, str.length() - start);
+        else
+            return str;
+    }
     public static void main(String[] args) {
-
+        System.out.println("middleThree(\"Candy\") = " + middleThree("Candy"));
+        System.out.println("middleThree(\"and\") = " + middleThree("and"));
+        System.out.println("middleThree(\"solving\") = " + middleThree("solving"));
     }
 }
