97e6e3f4d62c1cd046d1db071ba5d3bbd6ba2810
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-09 22:28:33 +0200

    feat: Ejercicios CodingBat String-1 WithoutEnd(instrucciones). Día 39

---
+++ src/_12repasoString1/_13WithoutEnd.java
@@ -0,0 +1,20 @@
+package _12repasoString1;
+
+public class _13WithoutEnd {
+    /*
+Given a string, return a version without the first and last char,
+so "Hello" yields "ell". The string length will be at least 2.
+
+withoutEnd("Hello") → "ell"
+withoutEnd("java") → "av"
+withoutEnd("coding") → "odin"
+     */
+    static String withoutEnd(String str) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("withoutEnd(\"Hello\") = " + withoutEnd("Hello"));
+        System.out.println("withoutEnd(\"java\") = " + withoutEnd("java"));
+        System.out.println("withoutEnd(\"coding\") = " + withoutEnd("coding"));
+    }
+}
