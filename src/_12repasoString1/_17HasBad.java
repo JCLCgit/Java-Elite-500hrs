41436dffefd49e97ee0de3ecc95510d910dbfbd4
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-11 14:22:53 +0200

    feat: Ejercicios CodingBat String-1 HasBad(completo). Día 42

--- src/_12repasoString1/_17HasBad.java
+++ src/_12repasoString1/_17HasBad.java
@@ -11,9 +11,17 @@ hasBad("xbadxx") → true
 hasBad("xxbadxx") → false
      */
     static boolean hasBad(String str) {
-
+        if(str.length() >= 3 && str.substring(0, 3).equals("bad")){
+            return true;
+        }
+        if(str.length() >= 4 && str.substring(1, 4).equals("bad")){
+            return true;
+        }
+        return false;
     }
     public static void main(String[] args) {
-
+        System.out.println("hasBad(\"badxx\") = " + hasBad("badxx"));
+        System.out.println("hasBad(\"xbadxx\") = " + hasBad("xbadxx"));
+        System.out.println("hasBad(\"xxbadxx\") = " + hasBad("xxbadxx"));
     }
 }
