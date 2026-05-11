bf9db8c7937337a677331bb5393c681effd1ef80
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-11 14:06:56 +0200

    feat: Ejercicios CodingBat String-1 HasBad(instrucciones). Día 41

---
+++ src/_12repasoString1/_17HasBad.java
@@ -0,0 +1,19 @@
+package _12repasoString1;
+
+public class _17HasBad {
+    /*
+Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0.
+Note: use .equals() to compare 2 strings.
+
+hasBad("badxx") → true
+hasBad("xbadxx") → true
+hasBad("xxbadxx") → false
+     */
+    static boolean hasBad(String str) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
