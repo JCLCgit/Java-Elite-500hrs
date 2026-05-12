a17505bdaa5ad6dd6e319c35035551562f10c9b2
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-12 20:25:05 +0200

    feat: Ejercicios CodingBat String-1 ConCat(completo). Día 43

---
+++ src/_12repasoString1/_18ConCat.java
@@ -0,0 +1,24 @@
+package _12repasoString1;
+
+public class _18ConCat {
+    /*
+Given two strings, append them together (known as "concatenation") and return the result.
+However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+conCat("abc", "cat") → "abcat"
+conCat("dog", "cat") → "dogcat"
+conCat("abc", "") → "abc"
+     */
+    static String conCat(String a, String b) {
+        if( !a.isEmpty() && !b.isEmpty()){
+            if (a.charAt(a.length() - 1) == b.charAt(0))
+                return a.substring(0, a.length() - 1) + b;
+        }
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("conCat(\"abc\", \"cat\") = " + conCat("abc", "cat"));
+        System.out.println("conCat(\"dog\", \"cat\") = " + conCat("dog", "cat"));
+        System.out.println("conCat(\"abc\", \"\") = " + conCat("abc", ""));
+    }
+}
