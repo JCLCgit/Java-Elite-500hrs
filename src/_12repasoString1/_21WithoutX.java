c876f0c3fd637bfb1213524ddd71f64c7782eaad
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-12 22:00:15 +0200

    feat: Ejercicios CodingBat String-1 WithoutX(completo). Día 43

---
+++ src/_12repasoString1/_21WithoutX.java
@@ -0,0 +1,30 @@
+package _12repasoString1;
+
+public class _21WithoutX {
+    /*
+Given a string, if the first or last chars are 'x',
+return the string without those 'x' chars, and otherwise return the string unchanged.
+
+withoutX("xHix") → "Hi"
+withoutX("xHi") → "Hi"
+withoutX("Hxix") → "Hxi"
+     */
+    static String withoutX(String str) {
+        if(str.length() == 1 && str.equals("x"))
+            return "";
+        if (str.length() >= 2){
+            if(str.charAt(0) == 'x' && str.charAt(str.length() - 1) == 'x')
+                return str.substring(1, str.length() - 1);
+            if(str.charAt(0) == 'x' && !(str.charAt(str.length() - 1) == 'x'))
+                return str.substring(1, str.length());
+            if(!(str.charAt(0) == 'x') && (str.charAt(str.length() - 1) == 'x'))
+                return str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
+    public static void main(String[] args) {
+        System.out.println("withoutX(\"xHix\") = " + withoutX("xHix"));
+        System.out.println("withoutX(\"xHi\") = " + withoutX("xHi"));
+        System.out.println("withoutX(\"Hxix\") = " + withoutX("Hxix"));
+    }
+}
