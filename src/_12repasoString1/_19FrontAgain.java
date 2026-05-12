78b23fad8172d353261a71322871ce634f2de3e3
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-12 21:00:15 +0200

    feat: Ejercicios CodingBat String-1 FrontAgain(completo). Día 43

---
+++ src/_12repasoString1/_19FrontAgain.java
@@ -0,0 +1,26 @@
+package _12repasoString1;
+
+public class _19FrontAgain {
+    /*
+Given a string, return true if the first 2 chars in the string also appear at
+the end of the string, such as with "edited".
+
+frontAgain("edited") → true
+frontAgain("edit") → false
+frontAgain("ed") → true
+     */
+    static boolean frontAgain(String str) {
+        if (str.length() < 2)
+            return false;
+        if (str.length() == 2)
+            return true;
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2)))
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println("frontAgain(\"edited\") = " + frontAgain("edited"));
+        System.out.println("frontAgain(\"edit\") = " + frontAgain("edit"));
+        System.out.println("frontAgain(\"ed\") = " + frontAgain("ed"));
+    }
+}
