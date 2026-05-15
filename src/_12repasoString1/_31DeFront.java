b790e611d02f0c47c3d806e4ca2ed446b596ab0d
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-15 22:54:18 +0200

    feat: Ejercicios CodingBat String-1 DeFront(instrucciones). Día 46

---
+++ src/_12repasoString1/_31DeFront.java
@@ -0,0 +1,20 @@
+package _12repasoString1;
+
+public class _31DeFront {
+    /*
+Given a string, return a version without the first 2 chars.
+Except keep the first char if it is 'a' and keep the second char if it is 'b'.
+The string may be any length. Harder than it looks.
+
+deFront("Hello") → "llo"
+deFront("java") → "va"
+deFront("away") → "aay"
+     */
+    static String deFront(String str) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
