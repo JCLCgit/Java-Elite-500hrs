8c527e98c79f8753088f8ac1d63a8e97ee721c15
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-16 21:07:22 +0200

    feat: Ejercicios CodingBat WarmUp-1 MixStart(completo y corregido). Día 47

--- src/_11repaso/_08MixStart.java
+++ src/_11repaso/_08MixStart.java
@@ -1,14 +1,19 @@
 package _11repaso;
 
 public class _08MixStart {
-    static boolean mixStart(String cadena){
-        return (cadena.strip().substring(1,3).equals("ix"));
+    /*
+Return true if the given string begins with "mix",
+except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false
+     */
+    static boolean mixStart(String str){
+        return (str.length() >= 3) && (str.trim().substring(1,3).equals("ix"));
     }
     public static void main(String[] args) {
 
-        String cadena = "mix clasicos";
-
-        System.out.println("cadena = " + mixStart(cadena));
         System.out.println("cadena = " + mixStart("mix snacks"));
         System.out.println("cadena = " + mixStart("pix snacks"));
         System.out.println("cadena = " + mixStart("piz snacks"));
