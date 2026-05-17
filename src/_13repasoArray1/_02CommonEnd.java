008fa192cc82d53c86731d74af705c344eb90616
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-17 21:35:59 +0200

    feat: Ejercicios CodingBat Array-1 CommonEnd(completo y comprobado). Día 48

---
+++ src/_13repasoArray1/_02CommonEnd.java
@@ -0,0 +1,25 @@
+package _13repasoArray1;
+
+public class _02CommonEnd {
+    /*
+Given 2 arrays of ints, a and b, return true if they have the same first element
+or they have the same last element. Both arrays will be length 1 or more.
+
+commonEnd([1, 2, 3], [7, 3]) → true
+commonEnd([1, 2, 3], [7, 3, 2]) → false
+commonEnd([1, 2, 3], [1, 3]) → true
+     */
+    static boolean commonEnd(int[] a, int[] b) {
+        return (a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1]);
+    }
+    public static void main(String[] args) {
+        int array1[] = new int[]{1, 2, 3};
+        int array2[] = new int[]{7, 3};
+        int array3[] = new int[]{7, 3, 2};
+        int array4[] = new int[]{1, 3};
+
+        System.out.println("commonEnd() = " + commonEnd(array1, array2));
+        System.out.println("commonEnd(array1, array3) = " + commonEnd(array1, array3));
+        System.out.println("commonEnd(array1, array4) = " + commonEnd(array1, array4));
+    }
+}
