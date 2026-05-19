d5c75700b57fd77a317b9b9b4cbae586718cdbd7
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-19 22:30:53 +0200

    feat: Ejercicios CodingBat Array-1 MakeMiddle(instrucciones). Día 50

---
+++ src/_13repasoArray1/_07MakeMiddle.java
@@ -0,0 +1,18 @@
+package _13repasoArray1;
+
+public class _07MakeMiddle {
+    /*
+Given an array of ints of even length, return a new array length 2 containing the middle
+two elements from the original array. The original array will be length 2 or more.
+
+makeMiddle([1, 2, 3, 4]) → [2, 3]
+makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+makeMiddle([1, 2]) → [1, 2]
+     */
+    static int[] makeMiddle(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
