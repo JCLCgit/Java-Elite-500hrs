b201d8a7c7230b99dc0d1e71e46df9b9aa4a3aba
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-19 22:16:08 +0200

    feat: Ejercicios CodingBat Array-1 Fix23(completo y corregido 1ra prueba). Día 50

--- src/_13repasoArray1/_06Fix23.java
+++ src/_13repasoArray1/_06Fix23.java
@@ -1,5 +1,7 @@
 package _13repasoArray1;
 
+import java.util.Arrays;
+
 public class _06Fix23 {
     /*
 Given an int array length 3, if there is a 2 in the array immediately followed by a 3,
@@ -9,10 +11,22 @@ fix23([1, 2, 3]) → [1, 2, 0]
 fix23([2, 3, 5]) → [2, 0, 5]
 fix23([1, 2, 1]) → [1, 2, 1]
      */
-    public int[] fix23(int[] nums) {
+    static int[] fix23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if ((i != nums.length - 1) && nums[i] == 2 && nums[i + 1] == 3){
+                nums[i + 1] = 0;
+                break;
+            }
+        }
+        return nums;
 
     }
     public static void main(String[] args) {
-
+        int[] array1 = new int[]{1, 2, 3};
+        int[] array2 = new int[]{2, 3, 5};
+        int[] array3 = new int[]{1, 2, 1};
+        System.out.println(Arrays.toString(fix23(array1)));
+        System.out.println(Arrays.toString(fix23(array2)));
+        System.out.println(Arrays.toString(fix23(array3)));
     }
 }
