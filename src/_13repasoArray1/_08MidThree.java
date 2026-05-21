be0d6844c1b1c68a823c2cc09e65aa31cc7ff53b
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-21 14:01:13 +0200

    feat: Ejercicios CodingBat Array-1 MidThree(instrucciones). Día 51

---
+++ src/_13repasoArray1/_08MidThree.java
@@ -0,0 +1,21 @@
+package _13repasoArray1;
+
+public class _08MidThree {
+    /*
+Given an array of ints of odd length, return a new array length 3
+containing the elements from the middle of the array. The array length will be at least 3.
+
+midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+midThree([1, 2, 3]) → [1, 2, 3]
+     */
+    public int[] midThree(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+        int[] array1 = new int[]{1, 2, 3, 4, 5};
+        int[] array2 = new int[]{8, 6, 7, 5, 3, 0, 9};
+        int[] array3 = new int[]{1, 2, 3};
+
+    }
+}
