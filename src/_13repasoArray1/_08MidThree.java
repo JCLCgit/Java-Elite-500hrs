4f48b90a531ce3d1030c23dc016089147aa2ddd8
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-21 14:10:42 +0200

    feat: Ejercicios CodingBat Array-1 MidThree(completo). Día 51

--- src/_13repasoArray1/_08MidThree.java
+++ src/_13repasoArray1/_08MidThree.java
@@ -1,5 +1,7 @@
 package _13repasoArray1;
 
+import java.util.Arrays;
+
 public class _08MidThree {
     /*
 Given an array of ints of odd length, return a new array length 3
@@ -9,13 +11,18 @@ midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
 midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
 midThree([1, 2, 3]) → [1, 2, 3]
      */
-    public int[] midThree(int[] nums) {
-
+    static int[] midThree(int[] nums) {
+        int position = nums.length / 2;
+        return new int[]{nums[position - 1], nums[position], nums[position]};
     }
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 3, 4, 5};
         int[] array2 = new int[]{8, 6, 7, 5, 3, 0, 9};
         int[] array3 = new int[]{1, 2, 3};
+
+        System.out.println("{1, 2, 3, 4, 5} = " + Arrays.toString(midThree(array1)));
+        System.out.println("{8, 6, 7, 5, 3, 0, 9} = " + Arrays.toString(midThree(array2)));
+        System.out.println("{1, 2, 3} = " + Arrays.toString(midThree(array3)));
 
     }
 }
