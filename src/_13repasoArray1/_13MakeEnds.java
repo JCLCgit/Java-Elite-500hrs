76efd20fe4fbf240ca0ce8c1ef5dde04ab28f433
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-22 21:34:17 +0200

    feat: Ejercicios CodingBat Array-1 MakeEnds(instrucciones). Día 52

---
+++ src/_13repasoArray1/_13MakeEnds.java
@@ -0,0 +1,19 @@
+package _13repasoArray1;
+
+public class _13MakeEnds {
+    /*
+Given an array of ints, return a new array length 2 containing the first
+and last elements from the original array.
+The original array will be length 1 or more.
+
+makeEnds([1, 2, 3]) → [1, 3]
+makeEnds([1, 2, 3, 4]) → [1, 4]
+makeEnds([7, 4, 6, 2]) → [7, 2]
+     */
+
+    public static void main(String[] args) {
+        int[] array1 = new int[]{1, 2, 3};
+        int[] array2 = new int[]{1, 2, 3, 4};
+        int[] array3 = new int[]{7, 4, 6, 2};
+    }
+}
