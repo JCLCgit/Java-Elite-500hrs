8b57bb9cd9f2c19fd46f1c5e4d2cb54e1705d7f8
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-22 21:28:43 +0200

    feat: Ejercicios CodingBat Array-1 MaxEnd3(completo y probado). Día 52

--- src/_13repasoArray1/_12MaxEnd3.java
+++ src/_13repasoArray1/_12MaxEnd3.java
@@ -1,5 +1,7 @@
 package _13repasoArray1;
 
+import java.util.Arrays;
+
 public class _12MaxEnd3 {
     /*
 
@@ -11,10 +13,19 @@ maxEnd3([1, 2, 3]) → [3, 3, 3]
 maxEnd3([11, 5, 9]) → [11, 11, 11]
 maxEnd3([2, 11, 3]) → [3, 3, 3]
      */
-    public int[] maxEnd3(int[] nums) {
-
+    static int[] maxEnd3(int[] nums) {
+        if (nums[0] >= nums[2]){
+            return new int[]{nums[0], nums[0], nums[0]};
+        }
+        return new int[]{nums[2], nums[2], nums[2]};
     }
     public static void main(String[] args) {
-
+        int[] array1 = new int[]{1, 2, 3};
+        int[] array2 = new int[]{11, 5, 9};
+        int[] array3 = new int[]{2, 11, 3};
+        System.out.println("{1, 2, 3} = " + Arrays.toString(maxEnd3(array1)));
+        System.out.println("{11, 5, 9} = " + Arrays.toString(maxEnd3(array2)));
+        System.out.println("{2, 11, 3} = " + Arrays.toString(maxEnd3(array3)));
+        
     }
 }
