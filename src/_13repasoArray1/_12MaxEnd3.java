7021c1e9b9d4772ca0ed53966bace950cc113ae4
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-22 20:50:50 +0200

    feat: Ejercicios CodingBat Array-1 MaxEnd3(instrucciones). Día 52

---
+++ src/_13repasoArray1/_12MaxEnd3.java
@@ -0,0 +1,20 @@
+package _13repasoArray1;
+
+public class _12MaxEnd3 {
+    /*
+
+Given an array of ints length 3, figure out which is larger,
+the first or last element in the array, and set all the other elements to be that value.
+Return the changed array.
+
+maxEnd3([1, 2, 3]) → [3, 3, 3]
+maxEnd3([11, 5, 9]) → [11, 11, 11]
+maxEnd3([2, 11, 3]) → [3, 3, 3]
+     */
+    public int[] maxEnd3(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
