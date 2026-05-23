e59129505ac21f7ec5297703ef94043ba6711a28
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-23 15:10:49 +0200

    feat: Ejercicios CodingBat Array-1 MakeLast(completo y probado). Día 53

--- src/_13repasoArray1/_14MakeLast.java
+++ src/_13repasoArray1/_14MakeLast.java
@@ -1,5 +1,7 @@
 package _13repasoArray1;
 
+import java.util.Arrays;
+
 public class _14MakeLast {
     /*
 Given an int array, return a new array with double the length where
@@ -11,7 +13,18 @@ makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
 makeLast([1, 2]) → [0, 0, 0, 2]
 makeLast([3]) → [0, 3]
      */
+    static int[] makeLast(int[] nums) {
+        int theLength = nums.length;
+        int[] result = new int[theLength*2];
+        result[result.length - 1] = nums[nums.length - 1];
+        return result;
+    }
     public static void main(String[] args) {
-
+        int[] array1 = new int[]{4, 5, 6};
+        int[] array2 = new int[]{1, 2};
+        int[] array3 = new int[]{3};
+        System.out.println("{4, 5, 6} = " + Arrays.toString(makeLast(array1)));
+        System.out.println("{1, 2} = " + Arrays.toString(makeLast(array2)));
+        System.out.println("{3} = " + Arrays.toString(makeLast(array3)));
     }
 }
