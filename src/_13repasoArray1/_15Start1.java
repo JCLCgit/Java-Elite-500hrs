e72f72ea0545d074d365f233b0d159dfc484d5f3
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-23 15:18:55 +0200

    feat: Ejercicios CodingBat Array-1 Start1(instrucciones). Día 53

---
+++ src/_13repasoArray1/_15Start1.java
@@ -0,0 +1,21 @@
+package _13repasoArray1;
+
+public class _15Start1 {
+    /*
+Start with 2 int arrays, a and b, of any length.
+Return how many of the arrays have 1 as their first element.
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], []) → 1
+     */
+    static int start1(int[] a, int[] b) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = new int[]{4, 5, 6};
+        int[] array2 = new int[]{1, 2};
+        int[] array3 = new int[]{3};
+    }
+}
