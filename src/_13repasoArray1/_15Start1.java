7bf4d1c24a169b481ce22989864d5c9ee182e988
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-24 16:09:07 +0200

    feat: Ejercicios CodingBat Array-1 Start1(completo y comprobado). Día 54

--- src/_13repasoArray1/_15Start1.java
+++ src/_13repasoArray1/_15Start1.java
@@ -1,5 +1,7 @@
 package _13repasoArray1;
 
+import java.util.Arrays;
+
 public class _15Start1 {
     /*
 Start with 2 int arrays, a and b, of any length.
@@ -11,11 +13,22 @@ start1([1, 2], []) → 1
      */
     static int start1(int[] a, int[] b) {
 
+        if((a.length > 0 && a[0] == 1) && (b.length > 0 && b[0] == 1))
+            return 2;
+        if((a.length > 0 && a[0] == 1) || (b.length > 0 && b[0] == 1))
+            return 1;
+        return 0;
     }
 
     public static void main(String[] args) {
-        int[] array1 = new int[]{4, 5, 6};
-        int[] array2 = new int[]{1, 2};
-        int[] array3 = new int[]{3};
+        int[] array1 = new int[]{1, 2, 3};
+        int[] array2 = new int[]{1, 3};
+        int[] array3 = new int[]{7, 2, 3};
+        int[] array4 = new int[]{1};
+        int[] array5 = new int[]{1, 2};
+        int[] array6 = new int[]{};
+        System.out.println("[1, 2, 3], [1, 3] = " + (start1(array1, array2)));
+        System.out.println("[7, 2, 3], [1] = " + (start1(array3, array4)));
+        System.out.println("[1, 2], [] = " + (start1(array5, array6)));
     }
 }
