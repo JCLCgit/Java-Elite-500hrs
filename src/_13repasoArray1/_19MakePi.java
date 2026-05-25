10fd9061deed5df4ebea2691cbd6b225f865b6d5
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-25 11:49:59 +0200

    feat: Ejercicios CodingBat Array-1 MakePi(completo). Día 55

---
+++ src/_13repasoArray1/_19MakePi.java
@@ -0,0 +1,19 @@
+package _13repasoArray1;
+
+import java.util.Arrays;
+
+public class _19MakePi {
+    /*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+
+     */
+    static int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
+    public static void main(String[] args) {
+        System.out.println("makePi() = " + Arrays.toString(makePi()));
+
+    }
+}
