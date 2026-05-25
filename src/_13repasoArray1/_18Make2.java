e17242cdb52c44860d5d7d61d9ada74c5d9f6f09
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-25 11:36:00 +0200

    feat: Ejercicios CodingBat Array-1 Make2(completo y eficiente). Día 55

--- src/_13repasoArray1/_18Make2.java
+++ src/_13repasoArray1/_18Make2.java
@@ -13,19 +13,46 @@ make2([4, 5], [1, 2, 3]) → [4, 5]
 make2([4], [1, 2, 3]) → [4, 1]
 make2([], [1, 2]) → [1, 2]
      */
+    /*
     static int[] make2(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        int[] resulFinal = new int[2];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            result[a.length + i] = b[i];
+        }
+        for (int i = 0; i < 2; i++) {
+            resulFinal[i] = result[i];
+        }
+        return resulFinal;
+    }
+     */
+    static int[] make2(int[] a, int[] b) {
+        int[] resultFinal = new int[2];
+        int index = 0;
+        for (int i = 0; i < a.length && index < 2; i++) {
+            resultFinal[index] = a[i];
+            index++;
+        }
+        for (int i = 0; i < b.length && index < 2; i++) {
+            resultFinal[index] = b[i];
+            index++;
+        }
 
+        return resultFinal;
     }
     public static void main(String[] args) {
-        int[] array1 = new int[]{1, 2, 3};
-        int[] array2 = new int[]{1, 5 ,3};
-        int[] array3 = new int[]{5, 2, 3};
+        int[] array1 = new int[]{4, 5};
+        int[] array2 = new int[]{1, 2 ,3};
+        int[] array3 = new int[]{4};
         int[] array4 = new int[]{1, 2, 3};
-        int[] array5 = new int[]{1, 5 ,3};
-        int[] array6 = new int[]{5, 2, 3};
+        int[] array5 = new int[]{};
+        int[] array6 = new int[]{1, 2};
 
         System.out.println("[4, 5], [1, 2, 3] = " + Arrays.toString(make2(array1, array2)));
-        System.out.println("[4, 5], [1, 2, 3] = " + Arrays.toString(make2(array1, array2)));
-        System.out.println("[4, 5], [1, 2, 3] = " + Arrays.toString(make2(array1, array2)));
+        System.out.println("[4], [1, 2, 3] = " + Arrays.toString(make2(array3, array4)));
+        System.out.println("[], [1, 2] = " + Arrays.toString(make2(array5, array6)));
     }
 }
