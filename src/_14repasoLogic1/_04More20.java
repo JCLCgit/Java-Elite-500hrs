d8d238797b8dda7a4fcdc94598ae0573d7e23b96
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-30 20:58:35 +0200

    feat: Ejercicios CodingBat Array-1 More20(completo y probado). Día 57

--- src/_14repasoLogic1/_04More20.java
+++ src/_14repasoLogic1/_04More20.java
@@ -9,7 +9,12 @@ more20(20) → false
 more20(21) → true
 more20(22) → true
      */
+    static boolean more20(int n) {
+        return (n % 20 == 1 || n % 20 == 2);
+    }
     public static void main(String[] args){
-
+        System.out.println("more20(20) = " + more20(20));
+        System.out.println("more20(21) = " + more20(21));
+        System.out.println("more20(22) = " + more20(22));
     }
 }
