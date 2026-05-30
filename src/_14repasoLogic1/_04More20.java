119338276bb437ad6e15694b91af833c6b437c75
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-30 20:54:21 +0200

    feat: Ejercicios CodingBat Array-1 More20(instrucciones). Día 57

---
+++ src/_14repasoLogic1/_04More20.java
@@ -0,0 +1,15 @@
+package _14repasoLogic1;
+
+public class _04More20 {
+    /*
+Return true if the given non-negative number is 1 or 2 more than a multiple of 20.
+See also: Introduction to Mod
+
+more20(20) → false
+more20(21) → true
+more20(22) → true
+     */
+    public static void main(String[] args){
+
+    }
+}
