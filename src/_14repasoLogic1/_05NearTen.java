2f14f4d35b2d0b7c6d179d57973e9c694422c87a
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-05-31 22:05:32 +0200

    feat: Ejercicios CodingBat Array-1 NearTen(completo y probado, opción muy directa). Día 58

--- src/_14repasoLogic1/_05NearTen.java
+++ src/_14repasoLogic1/_05NearTen.java
@@ -11,7 +11,12 @@ nearTen(12) → true
 nearTen(17) → false
 nearTen(19) → true
      */
+    static boolean nearTen(int num) {
+        return ((num % 10) == 2 || (num % 10) == 1 || (num % 10) == 9 || (num % 10) == 8 || (num % 10) == 0);
+    }
     public static void main(String[] args) {
-
+        System.out.println("nearTen(12) = " + nearTen(12));
+        System.out.println("nearTen(12) = " + nearTen(17));
+        System.out.println("nearTen(12) = " + nearTen(19));
     }
 }
