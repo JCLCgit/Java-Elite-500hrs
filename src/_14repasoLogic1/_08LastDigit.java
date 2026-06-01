ccdb0a74057f5155ee02c765bb776051c6e60774
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-06-01 10:52:59 +0200

    feat: Ejercicios CodingBat Logic-1 LastDigit(prueba1). Día 59

---
+++ src/_14repasoLogic1/_08LastDigit.java
@@ -0,0 +1,27 @@
+package _14repasoLogic1;
+
+public class _08LastDigit {
+    /*
+    Given three ints, a b c, return true if two or more of them have the same rightmost digit.
+    The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+
+lastDigit(23, 19, 13) → true
+lastDigit(23, 19, 12) → false
+lastDigit(23, 19, 3) → true
+     */
+    static boolean lastDigit(int a, int b, int c) {
+        int counter = 0;
+        if(a % 10 == b % 10)
+            counter++;
+        if((a % 10) == (c % 10))
+            counter++;
+        if(b % 10 == c % 10)
+            counter++;
+        return counter >= 1;
+    }
+    public static void main(String[] args) {
+        System.out.println("lastDigit(23, 19, 13) = " + lastDigit(23, 19, 13));
+        System.out.println("lastDigit(23, 19, 13) = " + lastDigit(23, 19, 12));
+        System.out.println("lastDigit(23, 19, 13) = " + lastDigit(23, 19, 3));
+    }
+}
