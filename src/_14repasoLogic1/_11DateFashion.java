3d89d99686f75175562c7fd3ad7b1704c04ed335
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-06-02 22:10:01 +0200

    feat: Ejercicios CodingBat Logic-1 DateFashion(completo y probado). Día 60

--- src/_14repasoLogic1/_11DateFashion.java
+++ src/_14repasoLogic1/_11DateFashion.java
@@ -15,8 +15,18 @@ dateFashion(5, 10) → 2
 dateFashion(5, 2) → 0
 dateFashion(5, 5) → 1
      */
-    public static void main(String[] args) {
+    static int dateFashion(int you, int date) {
+        if (you <= 2 || date <= 2)
+            return 0;
+        if (you >= 8 || date >= 8)
+            return 2;
 
+        return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println("dateFashion(5, 10) = " + dateFashion(5, 10));
+        System.out.println("dateFashion(5, 2) = " + dateFashion(5, 2));
+        System.out.println("dateFashion(5, 5) = " + dateFashion(5, 5));
     }
 
 }
