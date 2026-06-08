5b2e98b0befc0122a901154929a64f25b5b8af89
Juan Carlos López Carrillo <juancarlos.lopezcarrillo19@gmail.com>
2026-06-08 13:57:14 +0200

    feat: Ejercicios CodingBat Logic-1 SpecialEleven(in process). Día 63

---
+++ src/_14repasoLogic1/_23SpecialEleven.java
@@ -0,0 +1,19 @@
+package _14repasoLogic1;
+
+public class _23SpecialEleven {
+    /*
+We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+
+specialEleven(22) → true
+specialEleven(23) → true
+specialEleven(24) → false
+     */
+    static boolean specialEleven(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
